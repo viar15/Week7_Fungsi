31e41fc8620e0dc6438bf10bd8606fb85e46f6fa
Aiman Yoviar Maulana <145090629+viar15@users.noreply.github.com>
2023-10-23 15:10:36 +0700

    Add files via upload

---
+++ main (53).cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int
+reserve_num (int N)
+{
+  int bilangan[9], hasil[9], jumlahbilangan, jumlah = 0;
+
+  while (bilangan[jumlah] != '0')
+    {
+      jumlah++;
+    }
+  jumlahbilangan = jumlah - 1;
+
+    for (int i = 0; i < jumlah; i++) {
+      hasil[i] = bilangan[jumlahbilangan];
+      jumlahbilangan--;
+    }
+    
+    hasil[i] = '\0';
+
+int main ()
+  {
+    int jumlah[9];
+    for (int i = 0; i < 9; i++)
+      {
+      cin >> data;
+      cout << reserve_num << endl;
+      
+
+
+    return 0;
+  }
